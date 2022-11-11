b7e5e528a09ebf6eb09e31bdded09f974fa8bb8e
chulleyboy <ryanmmassenburg@gmail.com>
2022-11-10 23:46:09 -0800

    optimized code to no longer rely on the charCount variable, fixed user being prompted to reenter the password length when they clicked the cancel button, added many additional comments to the code

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//creates promps for user to specify parameters then generates password
 function generatePassword() {
   var passwordCriteria = {
     length: 0,
@@ -21,17 +22,25 @@ function generatePassword() {
     hasNumbers: false,
     hasSpecialCharacters: false
   }
+  //variable for writing the password
   var password =""
-  var charCount = 0
+  //array of all characters that are allowed within the password
   var characters = [];
+  //repeats the password length prompt until the under gives a valid answer
   while (!(passwordCriteria.length >= 8 && passwordCriteria.length <= 128)) {
     passwordCriteria.length = prompt ("What will be the length of the password?");
     if (!(passwordCriteria.length >= 8 && passwordCriteria.length <= 128)) {
-      alert ("Must be a number from 8 to 128")
+      if (passwordCriteria.length == null){
+        return "";
+      }
+      else {
+        alert ("Must be a number from 8 to 128")
+      }
     } else {
       alert ("The password will be " + passwordCriteria.length + " characters long")
     }
   }
+  //repeats the character used prompts until the user specifies at least one type of character to use
   while (passwordCriteria.hasLowercase == false && passwordCriteria.hasUppercase == false && passwordCriteria.hasNumbers == false && passwordCriteria.hasSpecialCharacters == false) {
     passwordCriteria.hasLowercase = confirm ("Will the password include lowercase characters?");
     if (passwordCriteria.hasLowercase == true){
@@ -61,54 +70,59 @@ function generatePassword() {
       alert ("Must include at least 1 type of character")
     }
   }
+  //add lowercase characters the characters array if the user wanted them
   if (passwordCriteria.hasLowercase == true){
     for (var i = 97; i < 123; i++){
-    characters[charCount++] = String.fromCharCode(i);
+    characters[characters.length] = String.fromCharCode(i);
     }
   }
+    //add uppercase characters the characters array if the user wanted them
   if (passwordCriteria.hasUppercase == true){
     for (var i = 65; i < 91; i++){
-    characters[charCount++] = String.fromCharCode(i);
+    characters[characters.length] = String.fromCharCode(i);
     }
   }
+    //add numbers the characters array if the user wanted them
   if (passwordCriteria.hasNumbers == true){
     for (var i = 48; i < 58; i++){
-    characters[charCount++] = String.fromCharCode(i);
+    characters[characters.length] = String.fromCharCode(i);
     }
   }
+    //add special characters the characters array if the user wanted them
   if (passwordCriteria.hasSpecialCharacters == true){
-    characters[charCount++] = "!";
-    characters[charCount++] = "@";
-    characters[charCount++] = "#";
-    characters[charCount++] = "$";
-    characters[charCount++] = "%";
-    characters[charCount++] = "^";
-    characters[charCount++] = "&";
-    characters[charCount++] = "*";
-    characters[charCount++] = "(";
-    characters[charCount++] = ")";
-    characters[charCount++] = "_";
-    characters[charCount++] = "-";
-    characters[charCount++] = "+";
-    characters[charCount++] = "=";
-    characters[charCount++] = "[";
-    characters[charCount++] = "{";
-    characters[charCount++] = "]";
-    characters[charCount++] = "}";
-    characters[charCount++] = ":";
-    characters[charCount++] = ";";
-    characters[charCount++] = "<";
-    characters[charCount++] = ",";
-    characters[charCount++] = ">";
-    characters[charCount++] = ".";
-    characters[charCount++] = "?";
-    characters[charCount++] = "/";
-    characters[charCount++] = "`";
-    characters[charCount++] = "~";
-    characters[charCount++] = "|";
+    characters[characters.length] = "!";
+    characters[characters.length] = "@";
+    characters[characters.length] = "#";
+    characters[characters.length] = "$";
+    characters[characters.length] = "%";
+    characters[characters.length] = "^";
+    characters[characters.length] = "&";
+    characters[characters.length] = "*";
+    characters[characters.length] = "(";
+    characters[characters.length] = ")";
+    characters[characters.length] = "_";
+    characters[characters.length] = "-";
+    characters[characters.length] = "+";
+    characters[characters.length] = "=";
+    characters[characters.length] = "[";
+    characters[characters.length] = "{";
+    characters[characters.length] = "]";
+    characters[characters.length] = "}";
+    characters[characters.length] = ":";
+    characters[characters.length] = ";";
+    characters[characters.length] = "<";
+    characters[characters.length] = ",";
+    characters[characters.length] = ">";
+    characters[characters.length] = ".";
+    characters[characters.length] = "?";
+    characters[characters.length] = "/";
+    characters[characters.length] = "`";
+    characters[characters.length] = "~";
+    characters[characters.length] = "|";
     }
+    //constructures the password using random characters from the characters array
   for (var i = 0; i < passwordCriteria.length; i++){
-    password = password + characters[Math.floor(Math.random()*(charCount-1)+0)];
+    password = password + characters[Math.floor(Math.random()*(characters.length))];
   }
   return password;
 
